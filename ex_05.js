ae680ad47173acaa6d95f03e71ffc2ffbcf7c072
Mars7attack <antoine.dufetelle@epitech.digital>
2024-09-30 13:25:19 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,3 @@
+let firstname = "John", lastname = "Doe", cardCode = "1111 2222 3333 4444", expiryDateMonth = 21,
+expiryDateYear = 2042, computedBankInfo = "John,Doe,1111 2222 3333 4444,21/2042";
+displayInCard(computedBankInfo)
