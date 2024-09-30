a780879fcabf00fe6470dc636e39d775001cffd7
Mars7attack <antoine.dufetelle@epitech.digital>
2024-09-30 15:30:15 +0200

    Create ex_09.js

---
+++ ex_09.js
@@ -0,0 +1,11 @@
+displayThisText("The variable value is " + input_var);
+if (Number.isInteger(input_var)) {
+	displayThisText("Its type is integer");
+} else if (Number.isFinite(input_var)) {
+	displayThisText("Its type is float");
+} else {
+	displayThisText("its type is " + typeof(input_var));
+}
+if (input_var == 42) {
+	displayThisText("It is the meaning of life");
+}
