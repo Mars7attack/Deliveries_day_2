ad77b7f4827e1a850011a47f8a15c661e037ec4a
Mars7attack <antoine.dufetelle@epitech.digital>
2024-09-30 12:30:40 +0200

    ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,9 @@
+let jerem, marceau = true, prince = 21, fatima = BigInt(33n), leane = 
+Symbol('baguette'), kyks = {age:18, monstre:true}, ayite = displayThisText;
+displayThisText(typeof(jerem));
+displayThisText(typeof(marceau));
+displayThisText(typeof(prince));
+displayThisText(typeof(fatima));
+displayThisText(typeof(leane));
+displayThisText(typeof(kyks));
+displayThisText(typeof(ayite));
