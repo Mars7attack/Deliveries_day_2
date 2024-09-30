e385f503a0c510a867256f037baa8af3477e9679
Mars7attack <antoine.dufetelle@epitech.digital>
2024-09-30 14:29:28 +0200

    Update ex_08.js

--- ex_08.js
+++ ex_08.js
@@ -1,7 +1,9 @@
 if (breadCount >= 2 && hamCount >= 1 && tunaCount >= 1) {
-	displayThisText("I can make a royal sandwich!")
+	displayThisText("I can make a royal sandwich!");
 } else if (breadCount >= 2 && hamCount >= 1) {
-  displayThisText("I can make a ham sandwich!")
+	displayThisText("I can make a ham sandwich!");
 } else if (breadCount >= 2 && tunaCount >= 1) {
-  displayThisText("I can make a tuna sandwich!")
-}
+	displayThisText("I can make a tuna sandwich!");
+} else { 
+	displayThisText("I'd rather be fasting tonight..")
+
